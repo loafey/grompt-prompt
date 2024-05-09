d96b866f4875e3ebd52b4da889cfa9be9c7f1f32
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-05-09 20:00:48 +0200

    Init.

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { flake-utils, nixpkgs, naersk, fenix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config = {
+            permittedInsecurePackages = [
+              "freeimage-unstable-2021-11-01"
+            ];
+          };
+        };
+
+        trenchbroom-working = pkgs.runCommand "trenchbroom"
+          { buildInputs = [ pkgs.makeWrapper pkgs.tree pkgs.gnused ]; } ''
+          makeWrapper ${pkgs.trenchbroom}/bin/trenchbroom $out/bin/trenchbroom --set QT_QPA_PLATFORM xcb
+        '';
+        toolchain = with fenix.packages.${system};  combine [
+          minimal.cargo
+          minimal.rustc
+          latest.clippy
+          latest.rust-src
+          latest.rustfmt
+        ];
+        min-pkgs = with pkgs; [
+          pkg-config
+          openssl
+          gcc
+        ];
+      in
+      {
+        defaultPackage = (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildPackage {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [ ] ++ min-pkgs;
+        };
+
+        devShell = (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        }).buildPackage {
+          src = ./.;
+          dontPatchELF = true;
+          nativeBuildInputs = with pkgs; [ ] ++ min-pkgs;
+        };
+      });
+}
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+#![feature(let_chains)]
+
+use owo_colors::OwoColorize;
+use std::env::current_dir;
+
+#[inline(always)]
+fn home_dir() -> String {
+    unsafe { std::env::var("HOME").unwrap_unchecked() }
+}
+
+#[inline(always)]
+fn get_path() -> String {
+    let path = unsafe { current_dir().unwrap_unchecked() };
+    let path = path.to_string_lossy();
+    let home = home_dir();
+    if path.starts_with(&home) {
+        if path == home {
+            String::new()
+        } else {
+            format!("{} ", path.replace(&home, "~"))
+        }
+    } else {
+        format!("{path} ")
+    }
+}
+
+#[inline(always)]
+fn get_grompt() -> String {
+    match grompt::format_status(grompt::options::get_options()) {
+        Ok(x) => format!("{x} "),
+        Err(_) => Default::default(),
+    }
+}
+
+#[inline(always)]
+fn get_emoji() -> char {
+    '🦦'
+}
+
+#[inline(always)]
+fn get() {
+    let path = get_path();
+    let grompt = get_grompt();
+    let emoji = get_emoji();
+    println!("{}{}{}", path.yellow().bold(), grompt, emoji);
+}
+
+fn main() {
+    // let max = 100;
+    // let mut total_time = 0.0f64;
+    // let mut slowest = 0.0f64;
+    // let mut fastest = f64::MAX;
+    // for _ in 0..max {
+    //     let time = std::time::Instant::now();
+    //     get();
+    //     let elapsed = time.elapsed().as_secs_f64();
+    //     total_time += elapsed;
+    //     slowest = slowest.max(elapsed);
+    //     fastest = fastest.min(elapsed);
+    // }
+    //
+    // let avg = total_time / max as f64;
+    //
+    // println!(
+    //     "Avg time was {avg}s ({}th of a second)",
+    //     (1.0 / avg) as usize
+    // );
+    // println!(
+    //     "Slowest time was {slowest}s ({}th of a second)",
+    //     (1.0 / slowest) as usize
+    // );
+    // println!(
+    //     "Fastest time was {fastest}s ({}th of a second)",
+    //     (1.0 / fastest) as usize
+    // );'
+    get();
+}
+
+// Avg time was 0.00138062763s
+// Slowest time was 0.061608917s
+// Fastest time was 0.000668868s
